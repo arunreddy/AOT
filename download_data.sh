8f50cce74bb3cf261bf6d5bbcbd5b74adc82845e
Arun Reddy <arunreddy.nelakurthi@gmail.com>
2018-02-07 18:01:44 -0700

    Added download script + dataset

--- download_data.sh
+++ download_data.sh
@@ -1,1 +1,7 @@
-mkdir -p ./data/raw
+mkdir -p ./raw
+cd ./raw
+
+echo "Office-Caltech data set"
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=10780rJdxSGvSvJJuhVPd9WtHVeVOFf1u' -O GFK_v1.tar.gz
+tar xvf GFK_v1.tar.gz
+rm GFK_v1.tar.gz
